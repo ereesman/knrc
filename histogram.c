c5ddd20e9b357dd77287c61fa178868f1592605f
Eddie Reesman <eddiereesman@gmail.com>
2016-11-09 11:01:48 -0800

    clean up unused variables, formatting

--- histogram.c
+++ histogram.c
@@ -1,69 +1,47 @@
 #include <stdio.h>
 
-#define TRUE  1
-#define FALSE 0
-int printedspace;
 int c;
 int count;
-int x;
-int y;
-int z;
-int a[25];
+int a[25]; // magic number, we assume 25 character words to be the max
 
-void getWordCountsByLength()
-{
+void getWordCountsByLength() {
   while ((c = getchar()) != EOF) {
     if (c != '\n' && c != ' ' && c != '\t') {
       count++;
     } else if (count > 0) {
       a[count]++;
-
       count = 0;
     }
   }  
 }
 
-void draw(int counts[])
-{
+void draw(int counts[]) {
   int mostFrequentWordCount = 0;
   int currentWordCount = 0;
   // determine height of grid to draw
   int i;
   int x;
-  for (i = 0;
-       i < 24;
-       i++)
-    {
-      currentWordCount = counts[i];
-      if (currentWordCount > mostFrequentWordCount)
-	{
-	  // we have a new most frequent count
-	  mostFrequentWordCount = currentWordCount;
-	}
+  for (i = 0; i < 24; i++) {
+    currentWordCount = counts[i];
+    if (currentWordCount > mostFrequentWordCount) {
+      // we have a new most frequent count
+      mostFrequentWordCount = currentWordCount;
     }
+  }
   // draw the histogram from top to bottom, left to right
-  for (i = mostFrequentWordCount;
-       i > 0;
-       i--)
-    {
-      for (x = 1;
-	   x < 25;
-	   x++)
-	{
-	  if (counts[x] >= i)
-	    {
-	      printf("*");
-	    } else
-	    {
-	      printf(" ");
-	    }
-	}
-      printf("\n");
+  for (i = mostFrequentWordCount; i > 0; i--) {
+    for (x = 1; x < 25; x++) {
+      if (counts[x] >= i) {
+	printf("*");
+      } else {
+	printf(" ");
+      }
     }
+    printf("\n");
+  }
 }
 
-int main()
-{
+int main() {
   getWordCountsByLength();
   draw(a);
 }
